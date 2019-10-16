4d20f6702cee7abb3c04c0d5da796f04ae52b59e
DiegoAneli <diegoaneli@gmail.com>
2019-10-16 19:34:22 +0200

    Create and verify signature

--- ecc.py
+++ ecc.py
@@ -189,7 +189,10 @@ class S256Field(FieldElement):
 A = 0 
 B = 7
 
-class S256Point():
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+
+class S256Point(Point):
 
     def __init__(self, x, y, a=None, b=None):
         a, b = S256Field(A), S256Field(B)
@@ -198,7 +201,14 @@ class S256Point():
         else:
             super().__init__(x=x, y=y, a=a, b=b)
 
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
 
+G = S256Point(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
 
 
 
@@ -241,9 +251,31 @@ class S256Point():
 
 
 
-###verifica con punti
+###verifica con punti,metodo punto G
 ##>>> gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
 ##>>> gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
 ##>>> p = 2**256 - 2**32 - 977
 ##>>> print(gy**2 % p == (gx**3 + 7) % p)
 ##True
+
+
+
+
+
+##########################SIGNATURE VERIFICATION############################
+
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return 'Signature({:x},{:x})'.format(self.r, self.s)
+
+#Type "help", "copyright", "credits" or "license" for more information.
+#>>> from ecc2 import G, N
+#>>> print(N*G)
+#S256Point(infinity)
+#>>> 
